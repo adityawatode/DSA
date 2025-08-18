fee688425c3db3888da48b8ea7a584d846b35de5
Aditya <adityashiv005@gmail.com>
2025-08-18 17:31:12 +0530

    Update FDS_assignment_1.c

--- FDS_assignment_1.c
+++ FDS_assignment_1.c
@@ -1,40 +1,95 @@
-// Define array of 1000 size Display the sum and index 10 number having the maximum sum
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-int main()
-{
-    int a[1000];
-    int i=0,j=0;
-    int max=0;
-    int value=0;
-    int max_index=0;
-    srand(time(NULL));
+#include <stdio.h>
+#include <stdlib.h>
 
-    for(i=0;i<1000;i++)
-    {
-    a[i]= rand()%1000+1;
-    }
+struct Node {
+    int data;
+    struct Node* next;
+};
 
-    for(i=0;i<1000;i++)
-    {
-        printf("%d\n", a[i]);
-    }
-    for(i=1;i<990;i++)
-    {
-    for(j=0;j<10;j++)
-    {
-        value= value + a[j];
-    }
-    value = value - a[i-1];
-    if(value>max)
-    {
-        max=value;
-        max_index=i;
-    }
-    }
-    printf("maximum sum of 10 consecutive number is %d\n", max);
-    printf("Index of maximum number is %d\n" , max_index);
-    return 0;
+struct Node* top = NULL;   // global stack top
+
+// Push (Create)
+void push(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = top;
+    top = newNode;
+    printf("%d pushed.\n", value);
 }
 
+// Display (Read)
+void display() {
+    if (top == NULL) {
+        printf("Stack is empty.\n");
+        return;
+    }
+    struct Node* temp = top;
+    printf("Stack: ");
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+// Pop (Delete)
+void pop() {
+    if (top == NULL) {
+        printf("Stack underflow!\n");
+        return;
+    }
+    struct Node* temp = top;
+    printf("%d popped.\n", temp->data);
+    top = top->next;
+    free(temp);
+}
+
+// Update (change value at a given position from top)
+void update(int pos, int newVal) {
+    struct Node* temp = top;
+    int i = 1;
+    while (temp != NULL && i < pos) {
+        temp = temp->next;
+        i++;
+    }
+    if (temp == NULL) {
+        printf("Invalid position.\n");
+    } else {
+        printf("Updated position %d from %d to %d.\n", pos, temp->data, newVal);
+        temp->data = newVal;
+    }
+}
+
+int main() {
+    int choice, val, pos;
+
+    while (1) {
+        printf("\n1. Push\n2. Display\n3. Pop\n4. Update\n5. Exit\nEnter choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter value: ");
+                scanf("%d", &val);
+                push(val);
+                break;
+            case 2:
+                display();
+                break;
+            case 3:
+                pop();
+                break;
+            case 4:
+                printf("Enter position from top: ");
+                scanf("%d", &pos);
+                printf("Enter new value: ");
+                scanf("%d", &val);
+                update(pos, val);
+                break;
+            case 5:
+                return 0;
+            default:
+                printf("Invalid choice.\n");
+        }
+    }
+}
